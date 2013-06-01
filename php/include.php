081b9bb08560820a2c5a1f8f97297f7f6067350d
Chris Gathercole <crisissdkrandom@gmail.com>
2013-06-02 00:11:43 +1000

    Added simple debug function. Writes out a var_dump in a <PRE> tag.

--- php/include.php
+++ php/include.php
@@ -1,8 +1,14 @@
 <?php
 	
+	function debug($stuff)
+	{
+		echo '<PRE>';var_dump($stuff);echo '</PRE>';
+	}
+	
 	function mysqlConnect()
 	{
 		return new mysqli('localhost', 'root', '', 'govhack');
 	}
+	
 	
  ?>
