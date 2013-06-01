d970497a01ca3d4cbc1446f411f7f87b79ca80c3
Leo Febey <overclockedamiga@gmail.com>
2013-06-01 22:15:27 +1000

    Getting used to git and added first AJAX php file

---
+++ php/ajax/get_biannual_usage_totals.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * @author: Leo
+ * About this code:
+ * Gets the bi-annual download totals for each state 
+ * in australia and converts it into a JSON file ready for
+ *  manipulation by Javascript
+ * 
+ * 
+ */
+?>
